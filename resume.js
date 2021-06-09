600cec5b827f37292de9b20ce2c4217a6c095e5e
Ryan Pearson <ryanpearson015@gmail.com>
2021-06-08 20:04:14 -0500

    completed draft of resume

---
+++ resume.js
@@ -0,0 +1,14 @@
+const fade = (element) => {
+    element.classList.add("animated", "fadeIn");
+  }
+  
+  const appear = (element) => {
+    element.style.color = "white";
+  }
+  
+  const disappear = (element) => {
+    element.hidden = true;
+  }
+  
+
+ 
